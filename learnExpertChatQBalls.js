8a8a558d968690f33af5b8648c0905cff24a6a2a
Kevin McCormack <wittyman37@yahoo.com>
2016-09-07 11:11:55 -0400

    refactor into functions, add balls to Qs right away

--- learnExpertChatQBalls.js
+++ learnExpertChatQBalls.js
@@ -7,18 +7,30 @@
 
 *****************************************************************/
 
-// Add the ball and redBall classes to the head
-$('<style>', {
-	type: 'text/css',
-	text:
-		'.ball{border:1px solid red;border-radius:50%;display:inline-block;padding:5px;}' +
-		'.redBall{ background:red } </style>").appendTo("head")'
-}).appendTo("head");
 
-window.setInterval(function(){
-  // Add red circle to each question node that will toggle filled in when clicked
+function addBallClassesToHead() {
+	// Add the ball and redBall classes to the head
+	$('<style>', {
+		type: 'text/css',
+		text:
+			'.ball{border:1px solid red;border-radius:50%;display:inline-block;padding:5px;}' +
+			'.redBall{ background:red } </style>").appendTo("head")'
+	}).appendTo("head");
+}
+
+function addBallsToQs() {
+	// Add red circle to each question node with one that will toggle filled in when clicked
 	$('.fc--question-node:not(:has(>.ball))').prepend($('<div>', {
 		"class":"ball",
 		onclick:"$(this).toggleClass('redBall')"
 	}));
-}, 30000); // runs every 30 seconds, first run delayed by 30s
+}
+
+// Run once
+addBallClassesToHead();
+addBallsToQs();
+
+window.setInterval(function(){
+	// runs every 30 seconds, first run delayed by 30s
+  addBallsToQs();
+}, 30000);
