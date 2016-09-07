9bd5549bb35421841f85e3aead1c29c5235efae9
Kevin McCormack <harlemsquirrel@gmail.com>
2016-09-06 20:29:43 -0400

    Add files via upload

---
+++ learnExpertChatQBalls.js
@@ -0,0 +1,19 @@
+/****************************************************************
+
+	********** HarlemSquirrel's **********
+	*** Learn Expert Chat Q-Balls Script ***
+	Run this script once when you load https://learn.co/expert-chat
+	and click the red ball on the Qs you'd like to watch
+
+*****************************************************************/
+	
+// Add the ball and redBall classes to the head
+$("<style type='text/css'> .ball{border:1px solid red;border-radius:50%;display:inline-block;padding:5px;} .redBall{ background:red } </style>").appendTo("head");
+
+window.setInterval(function(){
+  // Add red circle to each question node that will toggle filled in when clicked
+	$('.fc--question-node:not(:has(>.ball))').prepend($('<div>', {
+		"class":"ball",
+		onclick:"$(this).toggleClass('redBall')"
+	}));
+}, 30000); // runs every 30 seconds
